12b581d231094a6687f397e44020a021e0d0d452
Ife Ademolu-Odeneye <ife.ademoluodeneye@gmail.com>
2019-10-12 15:41:03 -0400

    begin writing defense

--- C_plus_plus/strategy.cpp
+++ C_plus_plus/strategy.cpp
@@ -15,3 +15,16 @@ void BasicOffense::enemyRobots(){
     } 
 };
 
+void BasicDefense::robots(){
+    int *p = ourRobots();
+    for (int n=0; n<6; n++) { //TODO: generalize for more than 6 robots
+        us[n] = *(p + n);
+    } 
+}
+
+void BasicDefense::enemyRobots(){
+    int *p = theirRobots();
+    for (int n=0; n<6; n++) {
+        enemy[n] = *(p + n);
+    } 
+}
