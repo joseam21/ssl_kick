892a81aad80d74b1c2a87f52be789e8c998a719d
Ian Perez <iperez@mit.edu>
2019-11-23 14:40:54 -0500

    Added NodeKey

--- rrtx/Node.cpp
+++ rrtx/Node.cpp
@@ -1,63 +1,105 @@
 //
-// Created by Ian Pérez on 10/26/19.
+// Created by Ian Pérez on 11/18/19.
 //
 
-#include "Node.h"
+#ifndef RRTXOPTIMIZEDVISUALIZATION_NODE_H
+#define RRTXOPTIMIZEDVISUALIZATION_NODE_H
 
-Node::Node(float xc, float yc){
-    xcor = xc;
-    ycor = yc;
 
-    incoming_0 = std::vector<Node*>();
-    outgoing_0 = std::vector<Node*>();
+#include <vector>
+#include <tuple>
+#include <limits>
+#include <cmath>
+#include <string>
+#include <iostream>
+#include <boost/unordered_map.hpp>
 
-    incoming_r = std::vector<Node*>();
-    outgoing_r = std::vector<Node*>();
+struct NodeKey {
+    float k1 = std::numeric_limits<float>::infinity();
+    float k2 = std::numeric_limits<float>::infinity();
 
-    g = std::numeric_limits<float>::infinity();
-    lmc = std::numeric_limits<float>::infinity();
-    parent = NULL;
-    children = std::vector<Node*>();
-}
-
-Node::Node(){
-    xcor = -100;
-    ycor = -100;
-}
-
-std::tuple<float, float> Node::getCoords(){
-    return std::make_tuple(xcor, ycor);
-}
-
-std::tuple<int, int> Node::getIntCoords(){
-    return std::make_tuple((int)xcor, (int)ycor);
-}
-
-float Node::distance(Node* other){
-    float other_x, other_y;
-    std::tie(other_x, other_y) = other->getCoords();
-    return std::sqrt(std::pow((xcor - other_x), 2) + std::pow((ycor - other_y), 2));
-}
-
-bool Node::operator==(const Node &other) const {
-    return this->xcor == other.xcor && this->ycor == other.ycor;
-}
-
-bool Node::operator!=(const Node &other) const {
-    return this->xcor != other.xcor || this->ycor != other.ycor;
-}
-
-bool Node::operator<(const Node &other) const {
-    if(this->xcor == other.xcor){
-        return this->ycor < other.ycor;
+    NodeKey(float first, float second){
+        k1 = first;
+        k2 = second;
     }
-    return this->xcor < other.xcor;
-}
+    NodeKey(){
+        k1 = std::numeric_limits<float>::infinity();
+        k2 = std::numeric_limits<float>::infinity();
+    }
 
-bool Node::operator>(const Node &other) const {
-    return true;
-}
+    bool operator<(const NodeKey key) const
+    {
+        return k1 < key.k1 || abs(k1 - key.k1) < 0.00001 && k2  < key.k2;
+    }
+    bool operator>(const NodeKey key) const
+    {
+        return k1 > key.k1 || abs(k1 - key.k1) < 0.00001 && k2  > key.k2;
+    }
+};
 
-void Node::display() {
-    std::cout << std::string("\n") + "(" + std::to_string(xcor) + ", " + std::to_string(ycor) + ")\n";
-}
+class Node {
+public:
+    float xcor;
+    float ycor;
+
+    NodeKey key;
+
+    std::vector<Node*> incoming_0;
+    std::vector<Node*> outgoing_0;
+
+    std::vector<Node*> incoming_r;
+    std::vector<Node*> outgoing_r;
+
+    float g;
+    float lmc;
+
+    Node* parent;
+    std::vector<Node*> children;
+
+    Node(float xc, float yc);
+    Node();
+    std::tuple<float, float> getCoords();
+    std::tuple<int, int> getIntCoords();
+    float distance(Node* other);
+    const float constDistance(const Node* other);
+    std::vector<Node*> inNeighbors();
+    std::vector<Node*> outNeighbors();
+    void display(); //For debugging purposes
+
+    bool operator==(const Node &other) const;
+    bool operator<(const Node &other) const;
+    bool operator>(const Node &other) const;
+    bool operator!=(const Node &other) const;
+
+};
+
+typedef std::pair<Node *, Node *> NodePair;
+struct hash_node_pair
+{
+    std::size_t operator()(const NodePair &p) const
+    {
+        std::size_t seed = 0;
+        seed += boost::hash<Node *>()(p.first);
+        seed ^= boost::hash<Node *>()(p.second);
+        return seed;
+    }
+};
+
+struct node_pair_equal
+{
+    bool operator()(const NodePair &p1, const NodePair &p2) const
+    {
+        return  p1.first == p2.first && p1.second == p2.second;
+    }
+};
+
+struct node_compare
+{
+    bool operator()(const Node *v1, const Node *v2) const
+    {
+        return v1->key > v2->key;
+    }
+};
+
+
+#endif //RRTXOPTIMIZEDVISUALIZATION_NODE_H
